755893fd6e9b60f1a753172d30db10272e4b2e2c
Serena <serena.f.yu@gmail.com>
2018-04-28 09:06:20 +1000

    bug fix

--- nGramSearch/nGramSearch.h
+++ nGramSearch/nGramSearch.h
@@ -82,7 +82,7 @@ public:
 	void init(std::unordered_map<str_t, std::pair<str_t, float>>& tempWordMap);
 	void getGrams(str_t* str);
 	void getGrams(const str_t& str, std::vector<str_t>& generatedGrams);
-	void getGrams(const char_t* str, const int size, std::vector<str_t>& generatedGrams);
+	//void getGrams(const char_t* str, const int size, std::vector<str_t>& generatedGrams);
 	void buildGrams();
 	size_t stringMatch(const str_t& query, const str_t& source, std::vector<size_t>& row1, std::vector<size_t>& row2);
 	void getMatchScore(const str_t& query, size_t lb, std::vector<str_t*>& targets, std::vector<float>& currentScore);
@@ -91,6 +91,8 @@ public:
 	void search(const char_t* query, const float threshold, const uint32_t limit, std::vector<str_t>& result);
 	void search(const char_t* query, char_t*** results, uint32_t* nStrings, const float threshold = 0, uint32_t limit = 100);
 	void release(char_t*** results, size_t nStrings);
+	void buildHash();
+	void getHash(str_t* str);
 
 	StringIndex& operator=(StringIndex<str_t>&& other)
 	{
@@ -127,10 +129,11 @@ protected:
 	std::vector<str_t> shortLib;
 	std::unordered_map<str_t*, std::pair<str_t, float>> wordMap;
 	std::unordered_map<str_t, std::vector<const str_t*>> ngrams;
+	std::unordered_map<str_t, std::vector<const str_t*>> approxHash;
 	int16_t gramSize = 3;
 
 private:
-	int sectionSize = 2000;
+	int sectionSize = 3000;
 };
 
 DLLEXP void index2D(char* const guid, char*** const key, const size_t size, const uint16_t gSize = 3, float* weight = NULL);
