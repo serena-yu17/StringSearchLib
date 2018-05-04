4e58f4b472b5c383b231109cb74a81dbc031c4c6
serena_yu <serena_yu@livingstone.com.au>
2018-05-04 16:24:20 +1000

    added filters

--- nGramSearch/nGramSearch.h
+++ nGramSearch/nGramSearch.h
@@ -24,16 +24,139 @@
 
 #define DLLEXP extern "C" __declspec(dllexport)
 
+
+/*
+Index the library based on a 2D array.
+@param key For each row, the first string is the key to be mapped to, and the second string is the description mapped to the key
+@param guid A unique id for the indexed library
+@param size size of the [key]
+@param gSize size of grams to be created. Default 3
+@param weight A list of the relative weight of each key. Default 1 for all
+*/
+DLLEXP void index2D(char* const guid, char*** const key, const uint64_t size, const uint16_t rowSize = 1, float** const weight = NULL,
+	char*** const attr = NULL, const uint16_t attrRowSize = 0, uint64_t* priceRange = NULL, const uint16_t gSize = 3);
+
+/*@{
+Index the library based on a 2D array. Wide string version
+@param key For each row, the first string is the key to be mapped to, and the second string is the description mapped to the key
+@param guid A unique id for the indexed library
+@param size size of the [key]
+@param gSize size of grams to be created. Default 3
+@param weight A list of the relative weight of each key. Default 1 for all
+@}*/
+DLLEXP void index2DW(char* const guid, wchar_t*** const key, const uint64_t size, const uint16_t rowSize = 1, float** const weight = NULL,
+	wchar_t*** const attr = NULL, const uint16_t attrRowSize = 0, uint64_t* priceRange = NULL, const uint16_t gSize = 3);
+
+/*@{
+Index the library based on a string array of key, and another array of additional text, e.g. description.
+Finally the additional will be mapped back to the keys.
+@param guid A unique id for the indexed library
+@param key keys to be searched for
+@param size size of the array for keys
+@param rowSize size of each additional text rows
+@param gSize size of grams to be created. Default 3
+@param weight A list of weight values for each key. It should be at least as long as the key array.
+@}*/
+DLLEXP void index(char* const guid, char** const key, const uint64_t size, const uint16_t rowSize = 1, float* const weight = NULL,
+	char** const attr = NULL, const uint16_t attrRowSize = 0, uint64_t* priceRange = NULL, const uint16_t gSize = 3);
+
+/*@{
+Wide string version to index the library based on a string array of key, and another array of additional text, e.g. description.
+Finally the additional will be mapped back to the keys.
+@param guid A unique id for the indexed library
+@param key keys to be searched for
+@param size size of the array for keys
+@param rowSize size of each additional text rows
+@param gSize size of grams to be created. Default 3
+@param weight A list of weight values for each key. It should be at least as long as the key array.
+@}*/
+DLLEXP void indexW(char* const guid, wchar_t** const key, const uint64_t size, const uint16_t rowSize, float* const weight = NULL,
+	wchar_t** const attr = NULL, const uint16_t attrRowSize = 0, uint64_t* priceRange = NULL, const uint16_t gSize = 3);
+
+/*@{
+search the query in the indexed library identified by the guid.
+@param guid A unique id for the indexed library
+@param query The query string
+@param results The pointer to a string array for output. The memory will be allocated by new
+@param nStrings Output the length of the results array.
+@param threshold lowest acceptable matching%, as a value between 0 and 1
+@param limit Maximum results generated, default 100
+@}*/
+DLLEXP void search(char* const guid, char* const query, char** const filters, int64_t* const filterSize, uint64_t* priceRange, char*** results, uint32_t* nStrings, const float threshold, const uint32_t limit);
+
+/*@{
+A wide string version to search the query in the indexed library identified by the guid.
+@param guid A unique id for the indexed library
+@param query The query string
+@param results The pointer to a string array for output. The memory will be allocated by new
+@param nStrings Output the length of the results array.
+@param threshold lowest acceptable matching%, as a value between 0 and 1
+@param limit Maximum results generated, default 100
+@}*/
+DLLEXP void searchW(char* const guid, wchar_t* const query, wchar_t** const filters, int64_t* const filterSize, uint64_t* priceRange, wchar_t*** results, uint32_t* nStrings, const float threshold, const uint32_t limit);
+
+/*@{
+To release the memory allocated for the result in the <search> function
+@param guid A unique id for the indexed library
+@param results The result returned by the <search> function.
+@param nStrings Length of <result>
+@}*/
+DLLEXP void release(char* const guid, char*** results, uint64_t nStrings);
+
+/*@{
+To release the memory allocated for the result in the <search> function. Wide string version.
+@param guid A unique id for the indexed library
+@param results The result returned by the <search> function.
+@param nStrings Length of <result>
+@}*/
+DLLEXP void releaseW(char* const guid, wchar_t*** results, uint64_t nStrings);
+
+/*@{
+To dispose a library indexed. If the library does not exist, <dispose> will ignore it.
+@param guid A unique id for the indexed library
+@}*/
+DLLEXP void dispose(char* const guid);
+
+/*@{
+Wide string version. To dispose a library indexed. If the library does not exist, <dispose> will ignore it.
+@param guid A unique id for the indexed library
+@}*/
+DLLEXP void disposeW(char* const guid);
+
+/*@{
+To obtain the current word map size
+@param guid A unique id for the indexed library
+@}*/
+DLLEXP uint64_t getSize(char* const guid);
+
+/*@{
+Wide string version. To obtain the current word map size
+@param guid A unique id for the indexed library
+@}*/
+DLLEXP uint64_t getLibSize(char* const guid);
+
+/*@{
+To obtain the current gram library size
+@param guid A unique id for the indexed library
+@}*/
+DLLEXP uint64_t getSizeW(char* const guid);
+
+/*@{
+Wide string version. To obtain the current gram library size
+@param guid A unique id for the indexed library
+@}*/
+DLLEXP uint64_t getLibSizeW(char* const guid);
+
 namespace
 {
 	//Allowed words for the query string. Other characters in the ASCII range will be converted to spaces
 	const std::unordered_set<char> wordChar
 	({
 		'.','%','$',' ',
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-	});
+		});
 
 	/*@{
 	Trims spaces in place from left of string
@@ -93,11 +216,11 @@ namespace
 	{
 		for (wchar_t& ch : str)
 			ch = towupper(ch);
-	} 
+	}
 
 	/*@{
 	Escapes all invalid characters to spaces
-	@param str String to be converted 
+	@param str String to be converted
 	@}*/
 	inline void escapeBlank(std::string& str)
 	{
@@ -138,16 +261,16 @@ public:
 	/*@{
 
 	@}*/
-	StringIndex(char_t** const key, const size_t size, const uint16_t rowSize, float* const weight, const uint16_t gSize);
-	StringIndex(char_t*** const key, const size_t size, const uint16_t rowSize, float** const weight, const uint16_t gSize);
-	StringIndex(std::vector<std::vector<str_t>>& key, const int16_t gSize, std::vector<std::vector<float>>& weight);
+	StringIndex(char_t** const key, const size_t size, const uint16_t rowSize, float* const weight, char_t** const attr, const uint16_t attrRowSize, size_t* priceRange, const uint16_t gSize);
+	StringIndex(char_t*** const key, const size_t size, const uint16_t rowSize, float** const weight, char_t*** const attr, const uint16_t attrRowSize, size_t* priceRange, const uint16_t gSize);
+	StringIndex(std::vector<std::vector<str_t>>& key, std::vector<std::vector<float>>& weight, std::vector<std::vector<str_t>>& attr, std::vector<std::vector<size_t>>& priceRange, const int16_t gSize);
 
 	StringIndex(StringIndex<str_t>&& other)
 	{
 		longLib = std::move(other.longLib);
 		shortLib = std::move(other.shortLib);
 		wordMap = std::move(other.wordMap);
-		ngrams = std::move(other.ngrams); 		
+		ngrams = std::move(other.ngrams);
 		weightVal = std::move(other.weightVal);
 		gramSize = other.gramSize;
 	}
@@ -165,20 +288,9 @@ public:
 		}
 	}
 
-	void init(std::unordered_map<str_t, std::vector<std::pair<str_t, float>>>& tempWordMap);
-	void getGrams(str_t* str);
-	void getGrams(const str_t& str, std::vector<str_t>& generatedGrams);
-	//void getGrams(const char_t* str, const int size, std::vector<str_t>& generatedGrams);
-	void buildGrams();
-	size_t stringMatch(const str_t& query, const str_t& source, std::vector<size_t>& row1, std::vector<size_t>& row2);
-	void getMatchScore(const str_t& query, size_t lb, std::vector<str_t*>& targets, std::vector<float>& currentScore); 
-	void searchShort(str_t& query, std::unordered_map<str_t*, float>& score);  
-	void searchLong(str_t& query, std::unordered_map<str_t*, float>& score);	
-	void search(const char_t* query, const float threshold, const uint32_t limit, std::vector<str_t>& result);
-	void search(const char_t* query, char_t*** results, uint32_t* nStrings, const float threshold = 0, uint32_t limit = 100);
+	void execSearch(char_t* const query, char_t** const filters, int64_t* const filterSize, size_t* priceRange, const float threshold, const uint32_t limit, std::vector<str_t>& result);
+	void search(char_t* const query, char_t** const filters, int64_t* const filterSize, size_t* priceRange, char_t*** results, uint32_t* nStrings, const float threshold = 0, const uint32_t limit = 100);
 	void release(char_t*** results, size_t nStrings) const;
-	/*void buildHash();
-	str_t getHash(str_t& str);*/
 	uint64_t size();
 	uint64_t libSize();
 
@@ -201,7 +313,7 @@ public:
 
 	// trim from both ends (in place)
 	template<class str_t>
-	static inline void trim(str_t &s) 
+	static inline void trim(str_t &s)
 	{
 		ltrim(s);
 		rtrim(s);
@@ -210,7 +322,11 @@ public:
 protected:
 	std::vector<str_t> longLib;
 	std::vector<str_t> shortLib;
-	std::unordered_map<str_t*, std::vector<std::pair<str_t, float>>> wordMap;
+	std::vector<str_t> keyLib;
+	std::unordered_map<str_t*, std::vector<str_t*>> wordMap;
+	std::unordered_map<str_t*, float> wordScore;
+	std::unordered_map<str_t*, std::vector<str_t>> keyAttr;
+	std::unordered_map<str_t*, std::pair<size_t, size_t>> keyPrice;
 	std::unordered_map<str_t, std::unordered_set<str_t*>> ngrams;
 	//std::unordered_map<str_t, std::vector<str_t*>> approxHash;
 	int16_t gramSize = 3;
@@ -219,126 +335,21 @@ private:
 	size_t sectionSize = 1000;
 	std::atomic<bool> indexed = false;
 	const float distanceFactor = 0.2f;
+	uint16_t attrLength = 0;
+	void filterScore(std::unordered_map<str_t*, float>& keyPair, std::unordered_map<str_t, float>& entryScore,
+		std::vector<std::unordered_set<str_t>>& filterVec, std::vector<bool>& filterStatus, size_t* priceRange, float threshold);
+	bool fitAttribute(std::vector<str_t>& attr, std::vector<std::unordered_set<str_t>>& filterVec, std::vector<bool>& filterStatus);
+	void init(std::unordered_map<str_t, std::vector<str_t>>& tempWordMap, std::unordered_map<str_t, float>& tempKeyScore,
+		std::unordered_map<str_t, std::vector<str_t>>& tempAttrLst, std::unordered_map<str_t, std::pair<size_t, size_t>>& tempKeyPrice);
+	void getGrams(str_t* str);
+	void getGrams(const str_t& str, std::vector<str_t>& generatedGrams);
+	//void getGrams(const char_t* str, const int size, std::vector<str_t>& generatedGrams);
+	void buildGrams();
+	size_t stringMatch(const str_t& query, const str_t& source, std::vector<size_t>& row1, std::vector<size_t>& row2);
+	void getMatchScore(const str_t& query, size_t lb, std::vector<str_t*>& targets, std::vector<float>& currentScore);
+	void searchShort(str_t& query, std::unordered_map<str_t*, float>& score);
+	void searchLong(str_t& query, std::unordered_map<str_t*, float>& score);
 };
-
-/*@{
-Index the library based on a 2D array.
-@param key For each row, the first string is the key to be mapped to, and the second string is the description mapped to the key
-@param guid A unique id for the indexed library
-@param size size of the [key]
-@param gSize size of grams to be created. Default 3
-@param weight A list of the relative weight of each key. Default 1 for all
-@}
-*/
-DLLEXP void index2D(char* const guid, char*** const key, const uint64_t size, const uint16_t rowSize = 1, float** const weight = NULL, const uint16_t gSize = 3);
-
-/*@{
-Index the library based on a 2D array. Wide string version
-@param key For each row, the first string is the key to be mapped to, and the second string is the description mapped to the key
-@param guid A unique id for the indexed library
-@param size size of the [key]
-@param gSize size of grams to be created. Default 3
-@param weight A list of the relative weight of each key. Default 1 for all
-@}*/
-DLLEXP void index2DW(char* const guid, wchar_t*** const key, const uint64_t size, const uint16_t rowSize = 1, float** const weight = NULL, const uint16_t gSize = 3);
-
-/*@{
-Index the library based on a string array of key, and another array of additional text, e.g. description.
-Finally the additional will be mapped back to the keys.
-@param guid A unique id for the indexed library
-@param key keys to be searched for
-@param size size of the array for keys
-@param rowSize size of each additional text rows
-@param gSize size of grams to be created. Default 3
-@param weight A list of weight values for each key. It should be at least as long as the key array.
-@}*/
-DLLEXP void index(char* const guid, char** const key, const uint64_t size, const uint16_t rowSize = 1, float* const weight = NULL, const uint16_t gSize = 3);
-
-/*@{
-Wide string version to index the library based on a string array of key, and another array of additional text, e.g. description.
-Finally the additional will be mapped back to the keys.
-@param guid A unique id for the indexed library
-@param key keys to be searched for
-@param size size of the array for keys
-@param rowSize size of each additional text rows
-@param gSize size of grams to be created. Default 3
-@param weight A list of weight values for each key. It should be at least as long as the key array.
-@}*/
-DLLEXP void indexW(char* const guid, wchar_t** const key, const uint64_t size, const uint16_t rowSize, float* const weight = NULL, const uint16_t gSize = 3);
-
-/*@{
-search the query in the indexed library identified by the guid.
-@param guid A unique id for the indexed library
-@param query The query string
-@param results The pointer to a string array for output. The memory will be allocated by new
-@param nStrings Output the length of the results array.
-@param threshold lowest acceptable matching%, as a value between 0 and 1
-@param limit Maximum results generated, default 100
-@}*/
-DLLEXP void search(char* const guid, const char* query, char*** results, uint32_t* nStrings, const float threshold = 0, uint32_t limit = 100);
-
-/*@{
-A wide string version to search the query in the indexed library identified by the guid.
-@param guid A unique id for the indexed library
-@param query The query string
-@param results The pointer to a string array for output. The memory will be allocated by new
-@param nStrings Output the length of the results array.
-@param threshold lowest acceptable matching%, as a value between 0 and 1
-@param limit Maximum results generated, default 100
-@}*/
-DLLEXP void searchW(char* const guid, const wchar_t* query, wchar_t*** results, uint32_t* nStrings, const float threshold = 0, uint32_t limit = 100);
-
-/*@{
-To release the memory allocated for the result in the <search> function
-@param guid A unique id for the indexed library
-@param results The result returned by the <search> function.
-@param nStrings Length of <result>
-@}*/
-DLLEXP void release(char* const guid, char*** results, uint64_t nStrings);
-
-/*@{
-To release the memory allocated for the result in the <search> function. Wide string version.
-@param guid A unique id for the indexed library
-@param results The result returned by the <search> function.
-@param nStrings Length of <result>
-@}*/
-DLLEXP void releaseW(char* const guid, wchar_t*** results, uint64_t nStrings);
-
-/*@{
-To dispose a library indexed. If the library does not exist, <dispose> will ignore it.
-@param guid A unique id for the indexed library
-@}*/
-DLLEXP void dispose(char* const guid);
-
-/*@{
-Wide string version. To dispose a library indexed. If the library does not exist, <dispose> will ignore it.
-@param guid A unique id for the indexed library
-@}*/
-DLLEXP void disposeW(char* const guid);
-
-/*@{
-To obtain the current word map size
-@param guid A unique id for the indexed library
-@}*/
-DLLEXP uint64_t getSize(char* const guid);
-
-/*@{
-Wide string version. To obtain the current word map size
-@param guid A unique id for the indexed library
-@}*/
-DLLEXP uint64_t getLibSize(char* const guid);
-
-/*@{
-To obtain the current gram library size
-@param guid A unique id for the indexed library
-@}*/
-DLLEXP uint64_t getSizeW(char* const guid);
-
-/*@{
-Wide string version. To obtain the current gram library size
-@param guid A unique id for the indexed library
-@}*/
-DLLEXP uint64_t getLibSizeW(char* const guid);
 
 #endif
 
