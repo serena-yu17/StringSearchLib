ad36a4cb2527c3624c5a20408bdb711673f934c5
Serena <serena.f.yu@gmail.com>
2018-05-19 11:12:04 +1000

    removed stdafx

--- nGramSearch/nGramSearch.h
+++ nGramSearch/nGramSearch.h
@@ -2,10 +2,6 @@
 #ifndef NGRAMSEARCH_H
 #define NGRAMSEARCH_H
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
-#include <windows.h>
-
 #include <string>
 #include <unordered_map>
 #include <mutex>
