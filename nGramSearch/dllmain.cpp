010c9b6183baa08f0cf6fe231773b67957335f7d
serena_yu <serena_yu@livingstone.com.au>
2018-07-30 13:23:50 +1000

    removed win32 macro

--- nGramSearch/dllmain.cpp
+++ nGramSearch/dllmain.cpp
@@ -1,6 +1,4 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
 #include "nGramSearch.h"
 #include "nGramSearch.hpp"
 
